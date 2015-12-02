f5b56317893bd7e93bfc16ff6da4889880189268
Matthias Fey <matthias.fey@tu-dortmund.de>
2015-12-02 17:40:53 +0100

    created test schemas

--- test/tests/i18n.js
+++ test/tests/i18n.js
@@ -1,11 +1,11 @@
 /* jshint node: true, mocha: true */
 'use strict';
 
-var mongoose = require('mongoose');
-var should = require('should');
+//var mongoose = require('mongoose');
+//var should = require('should');
 
 var helper = require('../helper');
-var i18nError = require('../../index');
+//var i18nError = require('../../index');
 
 module.exports = function() {
 
