21517f0ced52b51d1c4123a6ee75d05782005b8b
Matthias Fey <matthias.fey@tu-dortmund.de>
2015-12-02 17:43:09 +0100

    added test description

--- test/tests/i18n.js
+++ test/tests/i18n.js
@@ -1,15 +1,15 @@
 /* jshint node: true, mocha: true */
 'use strict';
 
-var mongoose = require('mongoose');
-var should = require('should');
+//var mongoose = require('mongoose');
+//var should = require('should');
 
 var helper = require('../helper');
-var i18nError = require('../../index');
+//var i18nError = require('../../index');
 
 module.exports = function() {
 
-	describe('Message', function() {
+	describe('I18n', function() {
 		afterEach(helper.afterEach);
 
 		it('', function(done) {
