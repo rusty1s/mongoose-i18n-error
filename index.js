68bd7d2baf1352d9170cf42df1f3f1c52a8f8543
Antoine Beauvais-Lacasse <antoine.beaulac@gmail.com>
2019-02-14 12:42:27 -0500

    make duplicate key error regex work with mongoose 5

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ module.exports = function(options) {
 		if (err.name !== 'MongoError') return null;
 		if (err.code !== 11000 && err.code !== 11001) return null;
 
-		var matches = /index:\s.*\.\$(.*)_1\sdup key:\s\{\s:\s"(.*)"\s\}/.exec(err.message);
+		var matches = /index:\s.*?\.?\$?(.*)_\d\sdup\s+key:\s*{\s*:\s*"(.*)"\s*}/.exec(err.message);
 
 		var result = {};
 		result[matches[1]] = {
